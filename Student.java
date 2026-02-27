77d0f2bacc15597b6ff9ca8c90d061d2880cce6f
Mohamed Ibrahim <mohamedibrahim9081@gmail.com>
2026-02-27 18:04:38 +0200

    Added student attributes and methods

---
+++ Student.java
@@ -0,0 +1,68 @@
+public class Student {
+    private int id ;
+    private String name;
+    private int numGrades;
+    private int maxnumGrades;
+    private double[] list_grades;
+
+    //empty constractor
+    public Student(){};
+
+
+    public Student(int id , String name , int maxnumGrades){
+        this.id=id;
+        this.name=name;
+        this.maxnumGrades = maxnumGrades;
+        numGrades = 0 ;
+        this.list_grades = new double[maxnumGrades];
+    }
+
+    public void ShowStudentinfo(){
+        System.out.println("ID: " + this.id + "   Name: " + this.name);
+    }
+
+    public void addGrade(double grade) {
+        if (numGrades >= maxnumGrades) {
+            System.out.println("Cannot add more grades – array is full!");
+            return;
+        }
+        list_grades[numGrades] = grade;
+        numGrades++;
+    }
+
+
+
+
+    public void printGrades() {
+        System.out.print("Grades: ");
+        for (int i = 0; i < numGrades; i++) {
+            System.out.print(list_grades[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+
+    public double getAverage(){
+        if(numGrades == 0 ) return 0.0;
+
+        double sum = 0.0 ;
+
+        for(int i = 0 ; i <numGrades ; i++){
+            sum += list_grades[i];
+        }
+
+        return sum / numGrades;
+    }
+
+
+   
+             
+        
+    }
+        
+
+
+
+    
+
